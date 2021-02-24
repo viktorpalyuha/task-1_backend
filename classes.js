7bc4b444b72793825745fa446eacf4fc5fd6a419
Viktor Palyuha <viktorpalyuha@gmail.com>
2021-02-24 16:19:27 +0200

    QuickSort added

--- classes.js
+++ classes.js
@@ -108,7 +108,49 @@ class SelectionSort {
   }
 }
 
-class QuickSort {}
+class QuickSort {
+  constructor(data) {
+    this.data = data;
+  }
+
+  sort(arr = this.data, start = 0, end = arr.length) {
+    function pivot(arr, start = 0, end = arr.length + 1) {
+      function swap(arr, firstIndex, secondIndex) {
+        if (arr[secondIndex]) {
+          const temp = arr[firstIndex];
+          arr[firstIndex] = arr[secondIndex];
+          arr[secondIndex] = temp;
+        }
+      }
+
+      let pivot = arr[start],
+        pointer = start;
+
+      for (let i = start; i < end; i++) {
+        if (
+          arr[i].make_display.toLowerCase() < pivot.make_display.toLowerCase()
+        ) {
+          pointer++;
+          swap(arr, pointer, i);
+        }
+      }
+      swap(arr, start, pointer);
+
+      return pointer;
+    }
+
+    let pivotIndex = pivot(arr, start, end);
+
+    if (start >= end) {
+      return arr;
+    }
+
+    this.sort(arr, start, pivotIndex);
+    this.sort(arr, pivotIndex + 1, end);
+
+    return arr;
+  }
+}
 
 class Search extends Algorithm {
   constructor() {
@@ -145,7 +187,7 @@ class InterpolationSearch {}
 
 module.exports.getData = (_, res) => {
   const sortFactory = new Sort();
-  let selection = sortFactory.create('selection');
+  let quick = sortFactory.create('quick');
 
-  res.status(200).json(selection.sort());
+  res.status(200).json(quick.sort());
 };

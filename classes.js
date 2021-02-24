d56ef6b46e2b95c68b31f617017480670d83baa4
Viktor Palyuha <viktorpalyuha@gmail.com>
2021-02-24 15:37:47 +0200

    SearchFactory added

--- classes.js
+++ classes.js
@@ -31,24 +31,47 @@ class Sort extends Algorithm {
   }
 }
 
-class BubbleSort {
+class BubbleSort {}
 
+class InsertionSort {}
+
+class SelectionSort {}
+
+class QuickSort {}
+
+class Search extends Algorithm {
+  constructor() {
+    super();
+  }
+  create(type) {
+    let algorithm;
+
+    switch (type) {
+      case 'linear':
+        algorithm = new LinearSearch();
+        break;
+      case 'binary':
+        algorithm = new BinarySearch();
+        break;
+      case 'jump':
+        algorithm = new JumpSearch();
+        break;
+      case 'interpolation':
+        algorithm = new InterpolationSearch();
+        break;
+    }
+    return algorithm;
+  }
 }
 
-class InsertionSort {
+class LinearSearch {}
 
-}
+class BinarySearch {}
 
-class SelectionSort {
+class JumpSearch {}
 
-}
-
-class QuickSort {
-
-}
+class InterpolationSearch {}
 
 module.exports.getData = (_, res) => {
-  res.status(200).json({
-    data: new Sort()
-  });
+  res.status(200).json(new Search());
 };

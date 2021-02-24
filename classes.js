78448f55db7792c818c416303d4ae77779b96d12
Viktor Palyuha <viktorpalyuha@gmail.com>
2021-02-24 16:07:20 +0200

    InsertionSort added

--- classes.js
+++ classes.js
@@ -55,7 +55,30 @@ class BubbleSort {
   }
 }
 
-class InsertionSort {}
+class InsertionSort {
+  constructor(data) {
+    this.data = data;
+  }
+
+  sort() {
+    for (let i = 1; i < this.data.length; i++) {
+      let currentElement = this.data[i];
+      let j = i - 1;
+
+      while (
+        j >= 0 &&
+        this.data[j].make_display.toLowerCase() >
+          currentElement.make_display.toLowerCase()
+      ) {
+        this.data[j + 1] = this.data[j];
+        j--;
+      }
+      this.data[j + 1] = currentElement;
+    }
+
+    return this.data;
+  }
+}
 
 class SelectionSort {}
 
@@ -96,7 +119,7 @@ class InterpolationSearch {}
 
 module.exports.getData = (_, res) => {
   const sortFactory = new Sort();
-  let bubble = sortFactory.create('bubble');
+  let insertion = sortFactory.create('insertion');
 
-  res.status(200).json(bubble.sort());
+  res.status(200).json(insertion.sort());
 };

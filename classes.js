616eef2f7c250ce42b18f8c33c2d83970e423061
Viktor Palyuha <viktorpalyuha@gmail.com>
2021-02-24 18:02:09 +0200

    BinarySearch added

--- classes.js
+++ classes.js
@@ -193,15 +193,40 @@ class LinearSearch {
   }
 }
 
-class BinarySearch {}
+class BinarySearch {
+  constructor(data) {
+    this.data = data;
+  }
+
+  search(target) {
+    let low = 0;
+    let high = this.data.length - 1;
+    let lowerCaseTarget = target.toLowerCase();
+    while (low <= high) {
+      const mid = Math.floor(low + high);
+      const element = this.data[mid].make_display.toLowerCase();
+      if (element < lowerCaseTarget) {
+        low = mid + 1;
+      } else if (element > lowerCaseTarget) {
+        high = mid - 1;
+      } else {
+        return this.data[mid];
+      }
+    }
+    return 'No target found';
+  }
+}
 
 class JumpSearch {}
 
 class InterpolationSearch {}
 
 module.exports.getData = (_, res) => {
-  const searchFactory = new Search();
-  let linear = searchFactory.create('linear');
+  const sortFactory = new Sort();
+  let quick = sortFactory.create('quick');
+  quick.sort();
 
-  res.status(200).json(linear.search('xedos'));
+  const searchFactory = new Search();
+  let binary = searchFactory.create('binary');
+  res.status(200).json(binary.search('xedos'));
 };

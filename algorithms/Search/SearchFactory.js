9a9cd348527d3617ac564422008c6bb86d7c53f5
Viktor Palyuha <viktorpalyuha@gmail.com>
2021-02-27 01:02:09 +0200

    rebased to interpolation

--- algorithms/Search/SearchFactory.js
+++ algorithms/Search/SearchFactory.js
@@ -2,7 +2,7 @@ const LinearSearch = require('./Linear');
 const BinarySearch = require('./Binary');
 const JumpSearch = require('./Jump');
 const TernarySearch = require('./Ternary');
-
+const InterpolationSearch = require('./Interpolation');
 class Search {
   static create(type) {
     let algorithm;
@@ -19,6 +19,9 @@ class Search {
         break;
       case 'ternary':
         algorithm = new TernarySearch();
+        break;
+      case 'interpolation':
+        algorithm = new InterpolationSearch();
         break;
     }
     return algorithm;

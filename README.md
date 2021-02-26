4a815a53a88149d91fafc15df7cc7f8f263e83db
Viktor Palyuha <viktorpalyuha@gmail.com>
2021-02-26 11:34:10 +0200

    Interpolation search

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# task-1_backend
+# task-2_backend
 
 In order to run server, please, do the following:
 
-1. Download the packages with the help of npm ('npm i');
-2. Run server with the help of 'npm start' command
+1. Use webpack in order to build app with 'npm run build' command;
+2. Run server with the help of 'npm run server' command.
+
+In case you need to run both server and front-end:
+
+1. Update submodules with 'git submodule update' command;
+2. Use webpack in order to build app with 'npm run build' command;
+3. Run app with the help of 'npm start' command.
--- algorithms/Search/Interpolation.js
+++ algorithms/Search/Interpolation.js
@@ -4,6 +4,35 @@ class InterpolationSearch extends Algorithm {
   constructor() {
     super();
   }
+
+  sort(target) {
+    let low = 0;
+    let high = this.algorithmData.length - 1;
+    let position = 0;
+    let delta = 0;
+
+    while (
+      low <= high &&
+      target >= this.algorithmData[low].make_display &&
+      target <= this.algorithmData[high].make_display
+    ) {
+      delta =
+        (target - this.algorithmData[low].make_display) /
+        (this.algorithmData[high].make_display - this.algorithmData[low].make_display);
+      position = low + Math.floor((high - low) * delta);
+      
+      if (this.algorithmData[position].make_display === target) {
+        return this.algorithmData[position];
+      }
+
+      if (this.algorithmData[position].make_display < target) {
+        low = position + 1;
+      } else {
+        high = position - 1;
+      }
+    }
+    return 'No target found';
+  }
 }
 
 module.exports = InterpolationSearch;

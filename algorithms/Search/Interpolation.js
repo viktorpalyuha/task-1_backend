58788dd7af707e85003decac87492d7668d98c2d
Viktor Palyuha <viktorpalyuha@gmail.com>
2021-02-26 15:06:05 +0200

    Interpolation renewed

--- algorithms/Search/Interpolation.js
+++ algorithms/Search/Interpolation.js
@@ -5,9 +5,49 @@ class InterpolationSearch extends Algorithm {
     super();
   }
 
-  sort(target) {
+  search(target) {
+    function countStr(str) {
+      let alphabet = [
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z'
+      ];
+
+      let splittedString = str.toLowerCase().split('');
+      let counter = 0;
+      for (let i = 0; i < 1; i++) {
+        let letterPosition = alphabet.indexOf(splittedString[i]) + 1;
+        counter += letterPosition;
+      }
+      return counter;
+    }
+
     let low = 0;
     let high = this.algorithmData.length - 1;
+    let countedTarget = countStr(target);
     let position = 0;
     let delta = 0;
 
@@ -16,11 +56,14 @@ class InterpolationSearch extends Algorithm {
       target >= this.algorithmData[low].make_display &&
       target <= this.algorithmData[high].make_display
     ) {
-      delta =
-        (target - this.algorithmData[low].make_display) /
-        (this.algorithmData[high].make_display - this.algorithmData[low].make_display);
+      delta = Math.abs(
+        (countedTarget - countStr(this.algorithmData[low].make_display)) /
+          countStr(this.algorithmData[high].make_display) -
+          countStr(this.algorithmData[low].make_display)
+      );
       position = low + Math.floor((high - low) * delta);
-      
+      console.log(position);
+
       if (this.algorithmData[position].make_display === target) {
         return this.algorithmData[position];
       }

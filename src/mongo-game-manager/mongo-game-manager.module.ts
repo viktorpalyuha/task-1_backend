06a0c829ef4389592e3786587a9dec0ba5f932b8
Viktor Palyuha <viktorpalyuha@gmail.com>
2021-03-09 16:05:28 +0200

    JSONParsingToMongo added

--- src/mongo-game-manager/mongo-game-manager.module.ts
+++ src/mongo-game-manager/mongo-game-manager.module.ts
@@ -1,12 +1,16 @@
 import { Game, GameSchema } from './schemas/game.schema';
 import { Module } from '@nestjs/common';
-import { MongoGameManagerController } from './mongo-game-manager.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+
+import { MongoGameManagerController } from './mongo-game-manager.controller';
+import { MongoGameManagerService } from './mongo-game-manager.service';
+import { MongoDatasetService } from './mongo-dataset/mongo-dataset.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Game.name, schema: GameSchema }]),
   ],
   controllers: [MongoGameManagerController],
+  providers: [MongoGameManagerService, MongoDatasetService],
 })
 export class MongoGameManagerModule {}

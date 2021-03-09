d1186e6ac725db5fe242a49a3d171c979db735de
Viktor Palyuha <viktorpalyuha@gmail.com>
2021-03-09 15:27:14 +0200

    Model for games created, mongoose added to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,22 @@
 import { Global, Module } from '@nestjs/common';
 import { RouterModule } from 'nest-router';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { routes } from './app-routing.module';
-import { DatasetService } from './dataset/dataset.service';
-import { GameManagerService } from './game-manager/game-manager.service';
 import { GameManagerModule } from './game-manager/game-manager.module';
 import { MongoGameManagerModule } from './mongo-game-manager/mongo-game-manager.module';
 
+import { AppService } from './app.service';
 @Global()
 @Module({
   imports: [
     RouterModule.forRoutes(routes),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/games', {
+      useNewUrlParser: true,
+    }),
     GameManagerModule,
     MongoGameManagerModule,
   ],
-  providers: [AppService, DatasetService, GameManagerService],
-  exports: [DatasetService, GameManagerService]
+  providers: [AppService],
 })
 export class AppModule {}
--- src/mongo-game-manager/mongo-game-manager.module.ts
+++ src/mongo-game-manager/mongo-game-manager.module.ts
@@ -1,7 +1,12 @@
+import { Game, GameSchema } from './schemas/game.schema';
 import { Module } from '@nestjs/common';
 import { MongoGameManagerController } from './mongo-game-manager.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Game.name, schema: GameSchema }]),
+  ],
   controllers: [MongoGameManagerController],
 })
 export class MongoGameManagerModule {}

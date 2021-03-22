d01f36e384fbd3c6e20c419d0f732a445b40e227
Viktor Palyuha <viktorpalyuha@gmail.com>
2021-03-22 13:49:52 +0200

    name of user.entity changed to customer.entity

--- src/postgres/auth/auth.module.ts
+++ src/postgres/auth/auth.module.ts
@@ -2,10 +2,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { User } from './entities/user.entity';
+import { Customer } from './entities/customer.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([Customer])],
   controllers: [AuthController],
   providers: [AuthService],
 })

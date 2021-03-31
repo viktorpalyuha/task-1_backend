481db63429ebde345f94c1598fd86ccfbfdcca75
Viktor Palyuha <viktorpalyuha@gmail.com>
2021-03-31 13:57:53 +0300

    possibility to store messages added

--- src/postgres/chat/chat.module.ts
+++ src/postgres/chat/chat.module.ts
@@ -1,10 +1,12 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './../auth/auth.module';
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
+import { Message } from './entities/message.entity';
 
 @Module({
-  imports: [AuthModule],
+  imports: [TypeOrmModule.forFeature([Message]), AuthModule],
   providers: [ChatGateway, ChatService],
 })
 export class ChatModule {}
